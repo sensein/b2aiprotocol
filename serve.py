301b5c9213d9b3278a87fbc8663f075ae972675e
ibevers <isaacbevers@gmail.com>
2024-10-08 15:26:53 -0400

    Configure to deploy through GitHub Actions

--- serve.py
+++ serve.py
@@ -23,7 +23,7 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
         self.end_headers()
 
 host = sys.argv[1] if len(sys.argv) > 2 else '0.0.0.0'
-port = int(sys.argv[len(sys.argv)-1]) if len(sys.argv) > 1 else 8000
+port = int(sys.argv[len(sys.argv)-1]) if len(sys.argv) > 1 else 8001
 
 print("Listening on {}:{}".format(host, port))
 httpd = HTTPServer((host, port), CORSRequestHandler)
