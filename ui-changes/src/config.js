c945a7f0254b5c7f605edda9dbbd1679bde1fe2d
ibevers <isaacbevers@gmail.com>
2024-10-09 14:19:30 -0400

    Fix the banner text

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     /* eslint-disable */
     githubSrc: 'https://raw.githubusercontent.com/sensein/b2aiprotocol/646fd415032f10da3892742dce984d76a3e1cc22/b2ai_protocol_schema',
-    banner: 'This is protocol for collecting data for the mood cohort of Bridge2AI at MIT.',
+    banner: 'This is the protocol for collecting data for the mood cohort of Bridge2AI Voice at MIT.',
     startButton: 'Start',
     assetsPublicPath: '/b2aiprotocol/',
     backendServer: 'http://3.145.21.200:8000/'
