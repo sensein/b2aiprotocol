c6675b28317cda8d859bacd8f0c8257815ecb1f1
ibevers <isaacbevers@gmail.com>
2024-10-09 13:55:06 -0400

    Update githubSrc link

--- ui-changes/config.js
+++ ui-changes/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     /* eslint-disable */
-    githubSrc: 'https://raw.githubusercontent.com/sensein/b2aiprotocol/refs/heads/gh-pages/b2ai_protocol_schema',
+    githubSrc: 'https://raw.githubusercontent.com/sensein/b2aiprotocol/646fd415032f10da3892742dce984d76a3e1cc22/b2ai_protocol_schema',
     banner: 'This is protocol for collecting data for the mood cohort of Bridge2AI at MIT.',
     startButton: 'Start',
     assetsPublicPath: '/b2aiprotocol/',
