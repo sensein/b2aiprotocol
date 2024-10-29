dac81d271149bff1e27a7483b4f19748e6241bd8
ibevers <isaacbevers@gmail.com>
2024-10-29 15:42:40 -0400

    Update config.js with local PTSD and ADHD checksum

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     /* eslint-disable */
-    githubSrc: 'https://raw.githubusercontent.com/sensein/b2aiprotocol/d877f8797120a7c00b1ed5bec697c212c704f814/b2ai_protocol_schema',
+    githubSrc: 'https://raw.githubusercontent.com/sensein/b2aiprotocol/0ff0381d34465a3f7b427fc9c78e061513205b45/b2ai_protocol_schema',
     banner: 'This is the protocol for collecting data for the mood cohort of Bridge2AI Voice at MIT.',
     startButton: 'Start',
     assetsPublicPath: '/b2aiprotocol/',
