d1ce6206183c2fda5090eadaf8e95d6b88db09ec
ibevers <isaacbevers@gmail.com>
2024-11-04 17:09:30 -0500

    Update deployed protocol checksum with unnested demographic optional items

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     /* eslint-disable */
-    githubSrc: 'https://raw.githubusercontent.com/sensein/b2aiprotocol/5c86f67b4ea6e37488420b801b5c2cddb0f1c50e/b2ai_protocol_schema',
+    githubSrc: 'https://raw.githubusercontent.com/sensein/b2aiprotocol/4f323ad07fd84a66e6c0ce83dbdd0e5a8e158702/b2ai_protocol_schema',
     banner: 'This is the protocol for collecting data for the mood cohort of Bridge2AI Voice at MIT.',
     startButton: 'Start',
     assetsPublicPath: '/b2aiprotocol/',
