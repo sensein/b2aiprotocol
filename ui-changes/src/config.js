7b7e394788d9f4943f5cf73050d2dcf00331cebf
ibevers <isaacbevers@gmail.com>
2024-11-27 14:54:27 -0500

    Update deployed checksum

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     /* eslint-disable */
-    githubSrc: 'https://raw.githubusercontent.com/sensein/b2aiprotocol/a9b5e6c3dbcc60f1833018e5b69743c3e6e1580a/b2ai_protocol_schema',
+    githubSrc: 'https://raw.githubusercontent.com/sensein/b2aiprotocol/2f0db0e19ba67f1f206f4670c9809a6b05ba146f/b2ai_protocol_schema',
     banner: 'This is the protocol for collecting data for the mood cohort of Bridge2AI Voice at MIT.',
     startButton: 'Start',
     assetsPublicPath: '/b2aiprotocol/',
