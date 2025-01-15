4eb6f0b4e33e329c84a8e2050c5433c648370267
ibevers <isaacbevers@gmail.com>
2025-01-15 12:55:02 -0500

    Update confounders b2ai-redcap2rs checksum

--- find_replace.py
+++ find_replace.py
@@ -2,9 +2,9 @@ import os
 
 # Define list of directories, target text, and replacement text
 directories = ["/Users/isaacbevers/sensein/reproschema-wrapper/b2ai-redcap2rs"]
-# search_text = "[LEGACY]\n"
-search_text = "https://raw.githubusercontent.com/sensein/b2ai-redcap2rs/24cbb461c2ff6556f047dd4bc1275b4b08d52eb8/activities/"
-replace_text = "\"NaN\""
+search_text = "[LEGACY]\\n"
+# search_text = "https://raw.githubusercontent.com/sensein/b2ai-redcap2rs/24cbb461c2ff6556f047dd4bc1275b4b08d52eb8/activities/"
+replace_text = ""
 
 # Iterate over each directory in the list
 for directory in directories:
@@ -12,7 +12,7 @@ for directory in directories:
         for file in files:
             file_path = os.path.join(root, file)
             suffixes = [".DS_Store", ".jpeg", "index", ".pack", ".idx"]
-            if not file_path.endswith(tuple(suffixes)):
+            if not file_path.endswith(tuple(suffixes)) and not ".git" in file_path:
                 # Open the file and replace text
                 print(file_path)
                 with open(file_path, 'r', encoding='utf-8') as f:
